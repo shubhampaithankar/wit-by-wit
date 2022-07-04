842fff5f515f667af0cbb77325e34fdf655ac860
Shubham-Paithankar <shubhampaithankar65@gmail.com>
2022-07-04 16:11:13 +0530

    improved form handling with useforms hooks

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import './Modal.scss'
 
@@ -7,32 +8,26 @@ import { UtilityService } from '../services'
 
 const Modal = ({ setTableData }: any) => {
 
+    const { register, handleSubmit, formState: {errors} } = useForm()
+
     const modalContext = useContext<any>(ModalContext)
     const { display: { show, onModalClose, modalData }  } = modalContext
     const modalRef = useRef<any>()
    
-    const props = { modalData }
+    const props = { modalData, register, errors }
 
-    const handleSubmit = (e: any) => {
-      e.preventDefault()
-
-      if (modalData.name !== 'remove') {
-        const formData = new FormData(e.target)
-        let name = formData.get('name')
-        let stdClass = formData.get('class')
-        let score = formData.get('score')
-
-        if (modalData.name === 'add') {
-          setTableData((prevState: any) => [...prevState, { name, class: stdClass, score }])
-        } else {
-          setTableData((prevState: any) => prevState.map((std: any) => std.name === name ? { ...std, name, class: stdClass, score } : std))
-        }
-      } else {
+    const onSubmit = (data: any) => {
+      if (modalData.name === 'remove') {
         setTableData((prevState: any) => {
           return prevState.filter((std: any) => std.name !== modalData.entry.name)
         })
+      } else {
+        if (modalData.name === 'add') {
+          setTableData((prevState: any) => [...prevState, { name: data.name, class: data.class, score: data.score }])
+        } else {
+          setTableData((prevState: any) => prevState.map((std: any) => std.name === data.name ? { ...std, name: data.name, class: data.class, score: data.score } : std))
+        }
       }
-
       onModalClose()
       return
     }
@@ -42,7 +37,7 @@ const Modal = ({ setTableData }: any) => {
     return (
       <div className="modal">
         <div ref={modalRef} className="modal-content px-3 pt-3">
-        <form onSubmit={e => handleSubmit(e)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="modal-head px-3">
             <h4 className="modal-title mb-2">{ modalData?.title }</h4>
           </div>
@@ -60,9 +55,10 @@ const Modal = ({ setTableData }: any) => {
       </div>
     )
 }
+
 export default Modal
 
-const ModalForm = ({ modalData }: any) => {
+const ModalForm = ({ modalData, register, errors }: any) => {
     const [score, setScore] = useState('')
 
     useEffect(() => {
@@ -73,22 +69,28 @@ const ModalForm = ({ modalData }: any) => {
       <>
         <div className="form-group mb-3">
           <label htmlFor="name" className='mb-2'>STUDENT NAME*</label>
-          <input type="text" name="name" className='form-control' 
-            // onChange={(e) => setName(e.target.value)}
+          <input type="text" name="name" className={`${errors?.name?.type === 'required' ? 'error' : ''} form-control`}
+            {...register('name', { required: true })}
             defaultValue={modalData.name === 'edit' ? modalData.entry.name : ''}/>
+          { errors?.name?.type === 'required' ? <p className='m-0 error-text'>Error: Name field cannot be left blank</p> : null }
         </div>
         <div className="form-group mb-3">
           <label htmlFor="class" className='mb-2'>CLASS*</label>
-          <input type="text" name="class" className='form-control'
-            // onChange={(e) => setStdClass(e.target.value)} 
+          <input type="text" name="class" className={`${errors?.class ? 'error' : ''} form-control`}
+            {...register('class', { required: true, min: 1, max: 12 })}
             defaultValue={modalData.name === 'edit' ? modalData.entry.class : ''}/>
+          { errors?.class ? <p className='m-0 error-text'>Error: Please input values between 1 and 12</p> : null }
+          { errors?.class?.type === 'required' ? <p className='m-0 error-text'>Error: Class field cannot be left blank</p> : null }
         </div>
         <div className="form-group mb-3">
           <label htmlFor="score" className='mb-2'>SCORE*</label>
-          <input type="text" name="score" className='form-control' 
+          <input type="text" name="score" className={`${errors?.score ? 'error' : ''} form-control`}
+            {...register('score', { required: true, min: 0, max: 100 })} 
             onChange={(e)=> setScore(e.target.value)} 
             defaultValue={modalData.name === 'edit' ? score : ''}
           />
+          { errors?.score?.type === ('max' || 'min') ? <p className='m-0 error-text'>Error: Please input values between 0 and 100</p> : null }
+          { errors?.score?.type === 'required' ? <p className='m-0 error-text'>Error: Score field cannot be left blank</p> : null }
         </div>
         <div className="form-group mb-3 d-flex flex-column">
           <label htmlFor="result" className='mb-2'>RESULT</label>
